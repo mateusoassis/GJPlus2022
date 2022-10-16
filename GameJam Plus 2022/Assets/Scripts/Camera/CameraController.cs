71096586ccf7621fc8904d2da4bf8650410684d9
DESKTOP-8PG3LDG\Mateus <mateusoassis@gmail.com>
2022-10-16 17:00:57 -0300

    Feature: putting the works on the camera going up

--- GameJam Plus 2022/Assets/Scripts/Camera/CameraController.cs
+++ GameJam Plus 2022/Assets/Scripts/Camera/CameraController.cs
@@ -6,4 +6,29 @@ public class CameraController : MonoBehaviour
 {
     [SerializeField] private Transform player;
     [SerializeField] private float maxYDistance;
+    [SerializeField] private float currentYDistance;
+    [SerializeField] private bool cameraMove;
+    [SerializeField] private float targetY;
+    [SerializeField] private float lerpCamera;
+
+    void Update()
+    {
+        currentYDistance = player.transform.position.y - transform.position.y;
+        if(currentYDistance < maxYDistance && !cameraMove)
+        {
+            cameraMove = true;
+            targetY = player.transform.position.y;
+        }
+
+        if(cameraMove)
+        {
+            LerpCameraY();
+        }
+    }
+
+    private void LerpCameraY()
+    {
+        Vector3 targetPos = new Vector3(transform.position.x, targetY, transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, targetPos, Time.deltaTime * lerpCamera);
+    }
 }
