e5760ea965a8d56d286e35367806dde2204541fe
DESKTOP-8PG3LDG\Mateus <mateusoassis@gmail.com>
2022-10-16 19:10:12 -0300

    Feature: pause all good

--- GameJam Plus 2022/Assets/Scripts/Scenes/FadeoutManager.cs
+++ GameJam Plus 2022/Assets/Scripts/Scenes/FadeoutManager.cs
@@ -5,6 +5,38 @@ using UnityEngine.UI;
 
 public class FadeoutManager : MonoBehaviour
 {
-    [SerializeField] private Image image;
+    [SerializeField] private GameObject loseGameFade;
+    [SerializeField] private float loseGameFadeDuration;
+    [SerializeField] private GameObject newGameFade;
+    [SerializeField] private float newGameFadeDuration;
+    [SerializeField] private float startTimer;
+
     [SerializeField] private YouLose youLoseScript;
+    [SerializeField] private PauseManager pauseManager;
+
+    void Start()
+    {
+        startTimer = newGameFadeDuration;
+        newGameFade.SetActive(true);
+        Time.timeScale = 1;
+    }
+
+    void Update()
+    {
+        if(startTimer > 0)
+        {
+            startTimer -= Time.deltaTime;
+        }
+        else
+        {
+            newGameFade.SetActive(false);
+        }
+        
+        if(youLoseScript.playerCollision)
+        {
+            loseGameFade.SetActive(true);
+            Time.timeScale = 0;
+            pauseManager.gamePaused = true;
+        }
+    }
 }
